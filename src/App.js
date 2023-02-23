63aafbdae4b7ad7bed01e77fba21ffce5975ff4e
MugaboDenys <mugabodan2020@gmail.com>
2023-02-23 19:30:22 +0200

    Hovering on image and mouse clicking

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col items-center mt-20">
+            <img src="https://picsum.photos/640/360" alt='' onMouseOver={()=>console.log("Hooh Haa!!! Hovered")} className='hover:brightness-50 duration-500' />
+            <button className='border shadow-md hover:shadow-lg mt-2 w-20 hover:bg-gray-400 duration-500 rounded-md' onClick={()=>console.log("I'm clicked")}>Click me</button>
+        </div>
     </div>
   );
 }
