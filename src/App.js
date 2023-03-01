38eae08b720dc08b6872c425befc72efd38649e7
MugaboDenys <mugabodan2020@gmail.com>
2023-03-01 16:55:08 +0200

    click challenge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [isImportant, setIsImportant] = React.useState("Yes")
+
+  function handleClick() {
+        setIsImportant("No")
+  }
+  
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+    <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value cursor-pointer" onClick={handleClick}>
+                <h1>{isImportant}</h1>
+            </div>
     </div>
   );
 }
