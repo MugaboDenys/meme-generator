dc51fa1e032ccadc592b541f2c72019e1261ff59
MugaboDenys <mugabodan2020@gmail.com>
2023-03-02 16:03:38 +0200

    tenary challenge

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  let isGoingOut = false;
-  
-  
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true
+})
+let starIcon = contact.isFavorite ? "../images/star-filled.png" : "../images/star-empty.png"
+function toggleFavorite() {
+  console.log("Toggle Favorite")
+}
   return (
 
     <div className="counter">
-            <button className="counter--minus" >–</button>
-            <div className="counter--count">
-                <h1>{`${isGoingOut ? "Yes" : "No"}`}</h1>
-            </div>
-            <button className="counter--plus">+</button>
+            <article className="card">
+                <img src="./images/user.png" alt='' className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={starIcon} 
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                        alt=""
+                    />
+                    <h2 className="card--name">
+                        {`${contact.firstName} ${contact.lastName}`}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+                
+            </article>
         </div>
   );
 }
