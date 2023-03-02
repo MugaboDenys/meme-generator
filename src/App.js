3559e74e8ea3188a654e4d0d38d5ff5aab4ca35d
MugaboDenys <mugabodan2020@gmail.com>
2023-03-02 14:26:23 +0200

    hover change state  exercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isGoingOut,setIsGoingOut] = useState(true)
+  function handleHover() {
+    setIsGoingOut(!isGoingOut)
+  }
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+    <div className="state">
+        <h1 className="state--title">Do I feel like going out tonight?</h1>
+        <div className="state--value" onMouseEnter={handleHover}  >
+            <h1>{`${isGoingOut ? "Yes" : "No"}`}</h1>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
