98aa3e5af67d75935c5585be572c8cc8d30be473
MugaboDenys <mugabodan2020@gmail.com>
2023-03-02 16:14:59 +0200

    add items challenge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
+    
+    function addItem() {
+        setThingsArray(prevState => {
+            return [...prevState, `Thing ${prevState.length + 1}`]
+        })
+    }
+    
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
+    <div>
+        <button onClick={addItem}>Add Item</button>
+        {thingsElements}
     </div>
   );
 }
